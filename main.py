169577247b39a399cfd0100bb047ea81c319d827
naomipoclava2021 <milagrito1dediciembre@gmail.com>
2022-10-08 19:37:34 -0300

    monkeypatch test unitario en clases

--- main.py
+++ main.py
@@ -1,6 +1,26 @@
+import re
+
+
 def suma(x,y):
     return x+y
 
+def resta(x,y):
+    return x - y
+
+def multiplicar(x,y):
+    return x * y
+
+def dividir(x,y):
+    return x / y
+    
 class Calculadora:
     def sumar(x,y):
         return suma(x,y)
+    def resta(x,y):
+        return resta(x,y)
+
+    def multiplicar(x,y):
+        return multiplicar(x,y)
+
+    def dividir(x,y):
+        return dividir(x,y)
--- test_class.py
+++ test_class.py
@@ -19,3 +19,18 @@ def test_class_monkey(monkeypatch):
     monkeypatch.setattr(Calculadora, "sumar", lambda x:4)
     c = Calculadora()
     assert c.sumar() == 4
+
+def test_class_resta(monkeypatch):
+    monkeypatch.setattr(Calculadora, "resta", lambda x:5)
+    c = Calculadora()
+    assert c.resta() == 5
+
+def test_class_multiplicar(monkeypatch):
+    monkeypatch.setattr(Calculadora, "multiplicar", lambda x:4)
+    c = Calculadora()
+    assert c.multiplicar() == 4
+
+def test_class_dividir(monkeypatch):
+    monkeypatch.setattr(Calculadora, "dividir", lambda x: 5)
+    c=Calculadora()
+    assert c.dividir() == 5
